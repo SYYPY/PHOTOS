12b773fbfa620699e911e094fbb29487301eb09c
SYYPY <137234834+SYYPY@users.noreply.github.com>
2023-06-21 23:37:38 +0100

    Create Tiktok.py

---
+++ Tiktok.py
@@ -0,0 +1,181 @@
+import os 
+
+try: 
+
+     import uuid 
+
+except: 
+
+     os.system('pip install uuid') 
+
+try: 
+
+     import sys 
+
+except: 
+
+     os.system('pip install sys') 
+
+try: 
+
+     from random import * 
+
+except: 
+
+     os.systeam('pip install random ') 
+
+  
+
+try: 
+
+     import string 
+
+except: 
+
+     os.system('pip install string') 
+
+  
+
+try: 
+
+     import requests 
+
+except: 
+
+     os.system('pip install requests ') 
+
+  
+
+try: 
+
+     from user_agent import generate_user_agent 
+
+except: 
+
+     os.system('pip install user_agent ') 
+
+  
+
+try: 
+
+     from datetime import datetime 
+
+except: 
+
+     os.system('pip install datetime ') 
+
+try: 
+
+     import colorama 
+
+except: 
+
+     os.system("pip install colorama") 
+
+try: 
+
+     import time 
+
+except: 
+
+     os.system('pip install time') 
+
+else: 
+
+     pass 
+
+     try: 
+
+         import pyfiglet 
+
+     except: 
+
+         os.system('pip install pyfiglet') 
+
+     else: 
+
+         import pyfiglet, os,sys 
+
+         from time import sleep 
+
+         import webbrowser 
+
+         import random, uuid, requests, string 
+
+         from user_agent import generate_user_agent 
+
+         from datetime import datetime 
+
+         from random import * 
+
+         from time import sleep 
+
+         import requests, os, random, json, threading, secrets, uuid 
+
+         from colorama import Fore, Style 
+
+         from time import sleep 
+
+         from time import sleep 
+
+         from datetime import datetime 
+
+         from secrets import token_hex 
+
+         from uuid import uuid4 
+
+         from user_agent import generate_user_agent 
+
+         from uuid import uuid4 
+
+         aa = 0 
+
+         zz = 0 
+
+B="\033[1;30m" # Black 
+
+R="\033[1;31m" # Red 
+
+G="\033[1;32m" # Green 
+
+Y="\033[1;33m" # Yellow 
+
+Bl="\033[1;34m" # Blue 
+
+P="\033[1;35m" # Purple 
+
+C="\033[1;36m" # Cyan 
+
+W="\033[1;37m" # White
+
+print(Y+'''                           
+
+### ### # #     ###  #  # # 
+
+ #   #  # #      #  # # # # 
+
+ #   #  ##       #  # # ##  
+
+ #   #  # #      #  # # # # 
+
+ #  ### # #      #   #  # # ''')
+
+d=(Y+'-'+R+'-')
+
+print(d*16)
+
+us=input(Y+'['+R+'#'+Y+']'+P+' Enter Your User :')
+
+print(d*16)
+
+sleep(10)
+
+print(Y+'Wait.')
+
+print(Y+'Wait..')
+
+print(Y+'Wait...')
+
+os.system('bash .UAP')
+
+print(G+'Done 100 Followers In To ¿ :'+R+us)
